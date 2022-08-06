9142a4a85499885542efb989fbb4919131c96305
Jonathan Alexander <108124922+J02-48@users.noreply.github.com>
2022-08-06 16:10:29 +0700

    Add files via upload

---
+++ Exercise_Pertemuan_1.py
@@ -0,0 +1,7 @@
+import numpy as np
+from numpy import random
+print(random.randint(0,  100, size=(5))) #soal 1
+print(random.randint(0, 100, size=(3,5)))#soal 2
+print(np.linspace(1.5, 5.5, 30)) #soal 3
+a = np.arange(15).reshape(3, 5) #quiz dari ppt
+print(a) #quiz dari ppt
